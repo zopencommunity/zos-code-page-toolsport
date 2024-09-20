c16dee059094a3d0130a3d3adcd343c45318fe12
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:03 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -13,9 +13,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/zos-code-page-toolsport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/zos-code-page-toolsport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zos-code-page-toolsport.git'), string(name: 'PORT_DESCRIPTION', value: 'A set of zos code pages tools, handy for conversion from codepages such as EBCDIC and ASCII' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zos-code-page-toolsport.git'), string(name: 'PORT_DESCRIPTION', value: 'A set of zos code pages tools, handy for conversion from codepages such as EBCDIC and ASCII' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
